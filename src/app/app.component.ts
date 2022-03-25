028f274e8ff241efb72945508611b76a302fe0c5
Llegian <llegian@valeo-it.com>
2022-03-25 13:42:51 +0200

    My Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  serverCreationStatus = "no server was created";
+  hide = true;
+  useDefault = false;
+  message = "Night mode off";
+
+  onCreateServer() {
+    this.serverCreationStatus = "server was created";
+  }
+
+  public toggle(event: MatSlideToggleChange) {
+    console.log('toggle', event.checked);
+    this.useDefault = event.checked;
+    if (this.useDefault==false)
+    this.message="Night mode off"
+    else
+    this.message="Night mode on" 
+  }
+
 }
+
+export class BackgroundimageComponent {  
+  constructor() { }   
+} 
+
+  
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider' ;
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule} from '@angular/material/icon';
+
+
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatDividerModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatIconModule,
+  
+
+  ],
+  providers: [],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {}
+
+
+
+
+
